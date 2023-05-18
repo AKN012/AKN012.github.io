90d4d41f4fc004325b090cf443ea88bb1fac1dff
AKN12 <123145598+AKN012@users.noreply.github.com>
2023-05-18 12:53:11 +0700

    e

--- js/booking.js
+++ js/booking.js
@@ -12,6 +12,12 @@ function submitResult() {
     return false;
 }
 
+document.getElementById('bookingInputs').addEventListener('submit', function (event) {
+    event.preventDefault();
+    var destination = document.getElementById('destination').value;
+    performHotelSearch(destination, checkIn, checkOut);
+});
+
 function elementBlur(input) {
     input.style.outline = '2px solid transparent';
     if (input.value.trim() === '') {
