1bd52f5404997f1356ca55d87674aa992b28bbaf
AKN12 <123145598+AKN012@users.noreply.github.com>
2023-05-18 19:43:50 +0700

    Update 1

--- js/booking.js
+++ js/booking.js
@@ -14,10 +14,39 @@ function submitResult() {
 
 document.getElementById('bookingInputs').addEventListener('submit', function (event) {
     event.preventDefault();
-    var destination = document.getElementById('destination').value;
-    performHotelSearch(destination, checkIn, checkOut);
+    doLocationInfo(destination.value);
 });
 
+function doLocationInfo() {
+    var data = [
+        {
+            value: 'Bali-Indonesia',
+            location: 'Bali, Indonesia',
+            price: 'P 1336',
+        },
+        {
+            value: 'Kyoto-Japan',
+            location: 'Kyoto, Japan',
+            price: 'P 2000',
+        },
+            ];
+        
+        var result = data.filter(function(item) {
+            return item.value === destination.value;
+        });
+            
+        if (result.length > 0) {
+            var output = "";
+            result.forEach(function(item) {
+            output += "Location: " + item.location + ", Price: " + item.price + "<br>";
+        });
+
+            document.getElementById("result").innerHTML = output;
+        } 
+        else {
+            document.getElementById("result").innerHTML = "No results found.";
+        }
+    }
 function elementBlur(input) {
     input.style.outline = '2px solid transparent';
     if (input.value.trim() === '') {
