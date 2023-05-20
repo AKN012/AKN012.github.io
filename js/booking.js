7888e49207f680a38eacd0879bbdb7d6a612dec6
AKN12 <123145598+AKN012@users.noreply.github.com>
2023-05-20 16:47:52 +0700

    Major Changes 3

--- js/booking.js
+++ js/booking.js
@@ -1,10 +1,16 @@
 let url = new URL(window.location.href);
 let params = new URLSearchParams(url.search);
 
-let destinationValue = params.get("destination");
+let destinationText = params.get("destination");
 let destination = document.getElementById("destination");
 
-destination.value = destinationValue;
+destination.value = destinationText;
+
+let destinationValue = destination.value;
+
+let locationFrom = document.getElementById("from");
+
+let locationFromValue = locationFrom.value; 
 
 let result = document.getElementById("resultDisplay");
 
@@ -24,27 +30,192 @@ function doLocationInfo() {
         {
             value: 'Bali-Indonesia',
             location: 'Bali, Indonesia',
-            price: 'P 1336',
+            timezone: '8',
         },
         {
             value: 'Kyoto-Japan',
             location: 'Kyoto, Japan',
-            price: 'P 2000',
+            timezone: '9',
+        },
+        {
+            value: 'Paris-France',
+            location: 'Paris France',
+        },
+        {
+            value: 'Santorini-Greece',
+            location: 'Santorini, Greece',
+        },
+        {
+            value: 'Tokyo-Japan',
+            location: 'Tokyo, Japan',
+        },
+        {
+            value: 'South-Africa',
+            location: 'South Africa',
+        },
+        {
+            value: 'Petra-Jordan',
+            location: 'Petra, Jordan',
+        },
+        {
+            value: 'Australia',
+            location: 'Australia',
+        },
+        {
+            value: 'Tanzania',
+            location: 'Tanzania',
+        },
+        {
+            value: 'Peru',
+            location: 'Peru',
+        },
+        {
+            value: 'New-York-City-USA',
+            location: 'New York City, USA',
+        },
+        {
+            value: 'Italy',
+            location: 'Italy',
+        },
+        {
+            value: 'Iceland',
+            location: 'Iceland',
+        },
+        {
+            value: 'Sydney-Australia',
+            location: 'Sydney, Australia',
+        },
+        {
+            value: 'Brazil',
+            location: 'Brazil',
+        },
+        {
+            value: 'Croatia',
+            location: 'Croatia',
+        },
+        {
+            value: 'Morocco',
+            location: 'Morocco',
+        },
+        {
+            value: 'Toronto-Canada',
+            location: 'Toronto, Canada',
+        },
+        {
+            value: 'Los-Angeles-USA',
+            location: 'Los Angeles, USA',
+        },
+        {
+            value: 'Beijing-China',
+            location: 'Beijing, China',
+        },
+        {
+            value: 'Bangkok-Thailand',
+            location: 'Bangkok, Thailand',
+        },
+        {
+            value: 'Manila-Philippines',
+            location: 'Manila, Philippines',
+        },
+        {
+            value: 'Seoul-South-Korea',
+            location: 'Seoul, South Korea',
+        },
+        {
+            value: 'Vatican-City',
+            location: 'Vatican-City',
+        },
+        {
+            value: 'Hong-Kong',
+            location: 'Hong Kong',
+        },
+        {
+            value: 'Maldives',
+            location: 'Maldives',
+        },
+        {
+            value: 'Hanoi-Vietnam',
+            location: 'Hanoi, Vietnam',
+        },
+        {
+            value: 'Kuala-Lumpur-Malaysia',
+            location: 'Kuala Lumpur, Malaysia',
+        },
+        {
+            value: 'Berlin-Germany',
+            location: 'Berlin, Germany',
+        },
+        {
+            value: 'Madrid-Spain',
+            location: 'Madrid, Spain',
+        },
+        {
+            value: 'Oslo-Norway',
+            location: 'Oslo, Norway',
+        },
+        {
+            value: 'Stockholm-Sweden',
+            location: 'Stockholm, Sweden',
+        },
+        {
+            value: 'Amsterdam-Netherlands',
+            location: 'Amsterdam, Netherlands',
+        },
+        {
+            value: 'Belgium',
+            location: 'Belgium',
         },
             ];
-        
-        var result = data.filter(function(item) {
-            return item.value === destination.value;
+        var resultTo = data.filter(function(to) {
+            return to.value === destination.value;
         });
+        var resultFrom = data.filter(function(from) {
+            return from.value === locationFrom.value;
+        });    
+        if (resultTo.length > 0 && resultFrom.length > 0) {
+            var timezoneFrom = resultFrom[0].timezone;
+            var timezoneTo = resultTo[0].timezone; 
+
+            let output = "<h1 style='color: black'> Ticket </h1>"
             
-        if (result.length > 0) {
-            var output = "";
-            result.forEach(function(item) {
-            output += "Location: " + item.location + " <br> Price: " + item.price + "<br>";
+            // Flight Times
+
+            let flightDuration = parseInt(document.getElementById("flightDura").value);
+            let flightStart = document.getElementById("flightStart").value;
+    
+            flightStart = convertToStandardTime(flightStart);
+            
+            output += "Flight Take Off: " + flightStart;
+            output += "<br> Flight Duration: " + flightDuration;
+    
+            let [hours, minutes] = flightStart.split(":");
+      
+            hours = parseInt(hours) - parseInt(timezoneFrom) + parseInt(timezoneTo) + parseInt(flightDuration);
+            minutes = parseInt(minutes);
+    
+            hours = hours % 24;
+      
+            let formattedHours = hours.toString().padStart(2, "0");
+            let formattedMinutes = minutes.toString().padStart(2, "0");
+      
+            let militaryTime = formattedHours + ":" + formattedMinutes;
+    
+            let flightArrival = convertToStandardTime(militaryTime);
+    
+            output += "<br> Flight Arrival: " + flightArrival;
+
+            // Flight Times End
+
+            resultFrom.forEach(function(from) {
+            output += "<br> From: " + from.location + "<br>";
+        });       
+            resultTo.forEach(function(to) {
+            output += "To: " + to.location + "<br>";
         });
 
             document.getElementById("result").innerHTML = output;
         } 
+
         else {
             document.getElementById("result").innerHTML = "No results found.";
         }
@@ -62,4 +233,21 @@ function elementBlur(input) {
 function elementFocus(input) {
     input.style.outline = '2px solid green';
     input.style.border = '2px solid transparent';
+}
+
+function convertToStandardTime(militaryTime) {
+    var hour = parseInt(militaryTime.substr(0, 2));
+    var minute = militaryTime.substr(3, 2);
+    var period = "AM";
+
+    if (hour === 0) {
+        hour = 12;
+    } else if (hour === 12) {
+        period = "PM";
+    } else if (hour > 12) {
+        hour -= 12;
+        period = "PM";
+    }
+
+return hour + ":" + minute + " " + period;
 }
